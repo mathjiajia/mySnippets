de1967d035ce19d664a05eafc84b1b20cf2b78f1
Jia Jia <jiajia2u@vip.qq.com>
2023-09-21 09:41:22 +0800

    feat(todo): thanks to `kunzaatko`

--- snippets/all.lua
+++ snippets/all.lua
@@ -1,24 +1,93 @@
-local snips = {}
-
-local context = require("mySnippets.context")
 local username = vim.env.USER:gsub("^%l", string.upper)
 
-snips = {
-	s({ trig = "todo", name = "TODO", dscr = "TODO:" }, {
-		t("TODO(" .. username .. "): "),
-	}, { condition = context.in_comments, show_condition = context.in_comments }),
+local calculate_comment_string = require("Comment.ft").calculate
+local utils = require("Comment.utils")
 
-	s({ trig = "fix", name = "FIXME", dscr = "FIXME:" }, {
-		t("FIXME(" .. username .. "): "),
-	}, { condition = context.in_comments, show_condition = context.in_comments }),
+--- Get the comment string {beg,end} table
+---@param ctype integer 1 for `line`-comment and 2 for `block`-comment
+---@return table comment_strings {begcstring, endcstring}
+local get_cstring = function(ctype)
+	local cstring = calculate_comment_string({ ctype = ctype, range = utils.get_region() }) or vim.bo.commentstring
+	local left, right = utils.unwrap_cstr(cstring)
+	return { left, right }
+end
 
-	s({ trig = "hack", name = "HACK", dscr = "HACK:" }, {
-		t("HACK(" .. username .. "): "),
-	}, { condition = context.in_comments, show_condition = context.in_comments }),
-
-	s({ trig = "note", name = "NOTE", dscr = "NOTE:" }, {
-		t("NOTE(" .. username .. "): "),
-	}, { condition = context.in_comments, show_condition = context.in_comments }),
+--- Options for marks to be used in a TODO comment
+local marks = {
+	signature = function()
+		return fmt("<{}>", i(1, username))
+	end,
+	date_signature = function()
+		return fmt("<{}{}>", { i(1, os.date("%d-%m-%y")), i(2, ", " .. username) })
+	end,
+	date = function()
+		return fmt("<{}>", i(1, os.date("%d-%m-%y")))
+	end,
+	empty = function()
+		return t("")
+	end,
 }
 
-return snips
+local todo_snippet_nodes = function(aliases, opts)
+	local aliases_nodes = vim.tbl_map(function(alias)
+		return i(nil, alias) -- generate choices for [name-of-comment]
+	end, aliases)
+	local sigmark_nodes = {} -- choices for [comment-mark]
+	for _, mark in pairs(marks) do
+		table.insert(sigmark_nodes, mark())
+	end
+	-- format them into the actual snippet
+	local comment_node = fmta("<> <>: <> <> <><>", {
+		f(function()
+			return get_cstring(opts.ctype)[1] -- get <comment-string[1]>
+		end),
+		c(1, aliases_nodes), -- [name-of-comment]
+		i(3), -- {comment-text}
+		c(2, sigmark_nodes), -- [comment-mark]
+		f(function()
+			return get_cstring(opts.ctype)[2] -- get <comment-string[2]>
+		end),
+		i(0),
+	})
+	return comment_node
+end
+
+--- Generate a TODO comment snippet with an automatic description and docstring
+---@param context table merged with the generated context table `trig` must be specified
+---@param aliases string[]|string of aliases for the todo comment (ex.: {FIX, ISSUE, FIXIT, BUG})
+---@param opts table merged with the snippet opts table
+local todo_snippet = function(context, aliases, opts)
+	opts = opts or {}
+	aliases = type(aliases) == "string" and { aliases } or aliases -- if we do not have aliases, be smart about the function parameters
+
+	opts.ctype = opts.ctype or 1 -- comment type can be passed in the `opts` table, but if it is not, we have to ensure, it is defined
+	local alias_string = table.concat(aliases, "|") -- `choice_node` documentation
+	context.name = context.name or (alias_string .. " comment") -- generate the `name` of the snippet if not defined
+	context.dscr = context.dscr or (alias_string .. " comment with a signature-mark") -- generate the `dscr` if not defined
+	context.docstring = context.docstring or (" {1:" .. alias_string .. "}: {3} <{2:mark}>{0} ") -- generate the `docstring` if not defined
+	local comment_node = todo_snippet_nodes(aliases, opts) -- nodes from the previously defined function for their generation
+	return s(context, comment_node, opts)
+end
+
+local todo_snippet_specs = {
+	{ { trig = "todo" }, "TODO" },
+	{ { trig = "fix" }, { "FIX", "BUG", "ISSUE", "FIXIT" } },
+	{ { trig = "hack" }, "HACK" },
+	{ { trig = "warn" }, { "WARN", "WARNING", "XXX" } },
+	{ { trig = "perf" }, { "PERF", "PERFORMANCE", "OPTIM", "OPTIMIZE" } },
+	{ { trig = "note" }, { "NOTE", "INFO" } },
+
+	{ { trig = "todob" }, "TODO", { ctype = 2 } },
+	{ { trig = "fixb" }, { "FIX", "BUG", "ISSUE", "FIXIT" }, { ctype = 2 } },
+	{ { trig = "hackb" }, "HACK", { ctype = 2 } },
+	{ { trig = "warnb" }, { "WARN", "WARNING", "XXX" }, { ctype = 2 } },
+	{ { trig = "perfb" }, { "PERF", "PERFORMANCE", "OPTIM", "OPTIMIZE" }, { ctype = 2 } },
+	{ { trig = "noteb" }, { "NOTE", "INFO" }, { ctype = 2 } },
+}
+
+local todo_comment_snippets = {}
+for _, v in ipairs(todo_snippet_specs) do
+	table.insert(todo_comment_snippets, todo_snippet(v[1], v[2], v[3]))
+end
+
+return todo_comment_snippets
