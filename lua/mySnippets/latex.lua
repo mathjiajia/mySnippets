492b9bbebb6d07ad883eee27ff212044075c80a2
Jia Jia <mathjiajia@outlook.com>
2024-06-24 10:34:50 +0800

    feat!: clean code

--- lua/mySnippets/latex.lua
+++ lua/mySnippets/latex.lua
@@ -25,16 +25,40 @@ local BULLET_ENVS = {
 	enumerate = true,
 }
 
+---@param col_offset? number
+---@return TSNode|nil
+local function get_node(col_offset)
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	row = row - 1 -- treesitter is 0-indexed
+	col = col + (col_offset or 0)
+
+	return vim.treesitter.get_node({ pos = { row, col } })
+end
+
+---@param node TSNode
+---@return string|nil
+local function get_environment(node)
+	local node_text = vim.treesitter.get_node_text(node, 0)
+	local first_line = vim.split(node_text, "\n")[1]
+	return first_line:match("\\begin{([^}]+)}")
+end
+
+---@param node TSNode
+---@return string|nil
+local function get_command(node)
+	local command = node:named_child(0)
+	if command then
+		return vim.treesitter.get_node_text(command, 0)
+	end
+end
+
 ---Check if cursor is in treesitter node of 'math_environment': 'tikzcd'
 ---@return boolean
 -- local function in_tikzcd()
 -- 	local node = vim.treesitter.get_node()
 -- 	while node do
 -- 		if node:type() == "generic_environment" then
--- 			local begin = node:child(0)
--- 			local names = begin and begin:field("name")
---
--- 			if names and names[1] and get_node_text(names[1], 0) == "tikzcd" then
+-- 			if get_environment(node) == "tikzcd" then
 -- 				return true
 -- 			end
 -- 		end
@@ -46,7 +70,7 @@ local BULLET_ENVS = {
 ---Check if cursor is in treesitter node of 'math'
 ---@return boolean
 local function in_math()
-	local node = vim.treesitter.get_node()
+	local node = get_node()
 	while node do
 		if node:type() == "text_mode" then
 			return false
@@ -67,15 +91,10 @@ end
 ---Check if cursor is in treesitter node of 'math_environment': 'align'
 ---@return boolean
 local function in_align()
-	local node = vim.treesitter.get_node()
+	local node = get_node()
 	while node do
-		if node:type() == "math_environment" then
-			local begin = node:child(0)
-			local names = begin and begin:field("name")
-
-			if names and names[1] and ALIGN_ENVS[get_node_text(names[1], 0):gsub("{(%w+)%s*%*?}", "%1")] then
-				return true
-			end
+		if node:type() == "math_environment" and ALIGN_ENVS[get_environment(node):gsub("%*$", "")] then
+			return true
 		end
 		node = node:parent()
 	end
@@ -83,15 +102,10 @@ local function in_align()
 end
 
 local function in_bullets()
-	local node = vim.treesitter.get_node()
+	local node = get_node()
 	while node do
-		if node:type() == "generic_environment" then
-			local begin = node:child(0)
-			local names = begin and begin:field("name")
-
-			if names and names[1] and BULLET_ENVS[get_node_text(names[1], 0):gsub("{(%w+)}", "%1")] then
-				return true
-			end
+		if node:type() == "generic_environment" and BULLET_ENVS[get_environment(node)] then
+			return true
 		end
 		node = node:parent()
 	end
@@ -101,13 +115,10 @@ end
 ---Check if cursor is in treesitter node of 'generic_command': '\xymatrix'
 ---@return boolean
 local function in_xymatrix()
-	local node = vim.treesitter.get_node()
+	local node = get_node()
 	while node do
-		if node:type() == "generic_command" then
-			local names = node:child(0)
-			if names and get_node_text(names, 0) == "\\xymatrix" then
-				return true
-			end
+		if node:type() == "generic_command" and get_command(node) == "xymatrix" then
+			return true
 		end
 		node = node:parent()
 	end
