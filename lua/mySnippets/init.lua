29f6fc3ec50a0d5c5bf4ccdc266ac9cf4aeb93fd
Jia Jia <mathjiajia@outlook.com>
2024-10-17 09:10:08 +0800

    feat!: change folder name

--- lua/mySnippets/init.lua
+++ lua/mySnippets/init.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.setup = function(opts)
-	local path = opts.path or vim.fn.stdpath("data") .. "/lazy/mySnippets/snippets"
+	local path = opts.path or vim.fn.stdpath("data") .. "/lazy/mySnippets/luasnippets"
 	require("luasnip.loaders.from_lua").lazy_load({ paths = path })
 end
 
